e3ce687ca517d8ed6dd313d3a2529012d17ff29b
Ryan Krauss <ryanlists@gmail.com>
2015-09-27 10:22:13 -0500

    debugging img 3

--- cherrypy_krauss_1.py
+++ cherrypy_krauss_1.py
@@ -72,7 +72,7 @@ class StringGenerator(object):
         self.zumo.kp = float(kwargs['Kp'])
         self.zumo.kd = float(kwargs['Kd'])
         self.zumo.run_test(N=500)
-        self.zumo.save('webtest')
+        self.zumo.save('webtest')#<-- probably need a data folder eventually
         #return cherrypy.lib.static.serve_file(os.path.abspath(self.zumo.data_file_name))
         #return str_out
         self.save_plot()
