f0cd390640eb516fefebe75a0e731967121eed2f
Ryan Krauss <ryanlists@gmail.com>
2015-10-22 22:17:37 -0500

    myserial for rpi

--- myserial.py
+++ myserial.py
@@ -1,6 +1,7 @@
 import serial_utils
 
-portname = '/dev/tty.usbmodem1421'
+#portname = '/dev/tty.usbmodem1421'
+portname = '/dev/ttyACM0'
 
 ser = serial_utils.Open_Serial(portname)
 ser.flushInput()
