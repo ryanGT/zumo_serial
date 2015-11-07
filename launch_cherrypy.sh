643d651f1e09954345090a791d877c42758f8d05
Ryan Krauss <ryanlists@gmail.com>
2015-11-07 10:57:24 -0600

    added git pull to launch_cherrpy.sh

--- launch_cherrypy.sh
+++ launch_cherrypy.sh
@@ -8,6 +8,7 @@ PYTHONPATH=$HOME/git/research:$HOME/git/krauss_misc:$HOME/git/bad
 #:/Users/rkrauss/src/scipy
 export PYTHONPATH
 cd /home/pi/zumo_serial
+git pull origin master >gitlog 2>&1
 sudo python get_ip.py >getiplog 2>&1
 sudo python cherrypy_krauss_1.py >cherrypylog 2>&1
 
