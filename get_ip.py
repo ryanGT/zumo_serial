d5bded71fe002f796d1ef3ee2dd23121d972cd91
Ryan Krauss <ryanlists@gmail.com>
2015-11-07 13:05:43 -0600

    adding 30 retries to getting a valid 192.168 ip

--- get_ip.py
+++ get_ip.py
@@ -5,6 +5,19 @@
 import ipfinder
 import time
 ipaddr = ipfinder.get_ip()
+
+tries = 0
+print('ipaddr = %s' ipaddr)
+
+while ipaddr.find('192.168') != 0:
+    tries += 1
+    print('tries = %i' % tries)
+    time.sleep(1)
+    ipaddr = ipfinder.get_ip()
+    if tries > 30:
+        break
+    
+
 stamp = "#" + time.strftime('%m/%d/%Y %H:%M:%S%p')
 filepath = '/home/pi/zumo_serial/ip.txt'
 f = open(filepath,'w')
