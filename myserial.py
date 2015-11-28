913a72648a7e82154a87a48554700fffa8c4559c
Ryan Krauss <ryanwkrauss@gmail.com>
2015-11-28 05:12:07 +0000

    trying to make myserial check ports in numeric order

--- myserial.py
+++ myserial.py
@@ -1,9 +1,20 @@
 import serial_utils
+import os
 
-portname = '/dev/ttyACM0'
+pat = '/dev/ttyACM%i'
+
+found_it = False
+
+for i in range(10):
+    portname = pat % i
+    if os.path.exists(portname):
+        found_it = True
+        break
+
 #portname = '/dev/ttyACM1'
 #portname = '/dev/cu.usbmodem1411'
 
-ser = serial_utils.Open_Serial(portname)
-ser.flushInput()
-ser.flushOutput()
+if found_it:
+    ser = serial_utils.Open_Serial(portname)
+    ser.flushInput()
+    ser.flushOutput()
