4a230213b9c6742f32a24b733e78c503529fdddf
Ryan Krauss <ryanwkrauss@gmail.com>
2016-02-25 13:26:40 -0600

    css tweaking

--- cherrypy_OL_phone.py
+++ cherrypy_OL_phone.py
@@ -106,19 +106,19 @@ class StringGenerator(object):
             
         out = self.top_header + \
               """<form method="get" action="forward">
-              <button type="submit"><img src="/img/forward.png" alt="Forward"></button>
+              <button type="submit" style="font-size:60px;min-width: 200px; width:300px;"><img src="/img/forward.png" alt="Forward"></button>
               </form>
               <form method="get" action="forward">
-              <button type="submit" style="font-size:40px;min-width: 50px;width: 80px;">Forward</button>
+              <button type="submit" style="font-size:60px;min-width: 200px; width:300px;">Forward</button>
               </form>
               <form method="get" action="left">
-              <button type="submit" style="font-size:60px;min-width: 100px;width: 200px;">Left</button>
+              <button type="submit" style="font-size:60px;min-width: 200px;width: 300px;">Left</button>
               </form>
               <form method="get" action="right">
-              <button type="submit" style="font-size:80px">Right</button>
+              <button type="submit" style="font-size:60px;min-width: 200px; width:300px;">Right</button>
               </form>
               <form method="get" action="back">
-              <button type="submit">Back</button>
+              <button type="submit" style="font-size:60px;min-width: 200px; width:300px;">Back</button>
               </form>"""
         return out
     
